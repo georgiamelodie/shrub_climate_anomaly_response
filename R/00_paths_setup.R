4b1bb8455c6746901b25cc39e0b446a01a3462d4
HOLE <khth73@durham.ac.uk>
2026-02-18 11:29:30 +0000

    update code

--- R/00_paths_setup.R
+++ R/00_paths_setup.R
@@ -1,23 +1,19 @@
 ##############################################################################
 # Alnus and Salix shrub anatomical response to climate
-##Author: Georgia Hole
 ##scripts/00_paths_setup.R
-
-source("R/utils_packages.R")
-load_project_packages()
 
 # Root project directory
 root <- here::here()
 
-# Define subfolders
-data_dir    <- file.path(root, "Data")
-results_dir <- file.path(root, "results")
-figs_dir     <- file.path(root, "figures")
+# Define subfolders 
+data_dir    <- file.path(root, "data")
+output_dir  <- file.path(root, "output")
+figures_dir <- file.path(root, "figures")
 
-dir.create(results_dir, showWarnings = FALSE, recursive = TRUE)
-dir.create(figs_dir,     showWarnings = FALSE, recursive = TRUE)
+dir.create(output_dir,  showWarnings = FALSE, recursive = TRUE)
+dir.create(figures_dir, showWarnings = FALSE, recursive = TRUE)
 
 message("Project root: ", root)
 message("Data dir: ", data_dir)
-message("Results dir: ", results_dir)
-message("Figures dir: ", figs_dir)
+message("Output dir: ", output_dir)
+message("Figures dir: ", figures_dir)
